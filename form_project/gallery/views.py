c0c7fc8a9267a201adaa1020418b0ab51ed2a0d8
andrei <a.griaznovskiy@gmail.com>
2022-11-06 17:02:45 +0300

    Created file upload to the gallery

--- form_project/gallery/views.py
+++ form_project/gallery/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.views import View
 
+
+
 # Create your views here.
+
+def storage_file(file):
+    file_name = file.name
+    with open(f'gallery_tmp/{file_name}.jpg', 'wb+') as new_file:
+        for chunk in file.chunks():
+            new_file.write(chunk)
+
 
 class GalleryView(View):
 
@@ -9,4 +18,5 @@ class GalleryView(View):
         return render(request, 'gallery/load_file.html')
 
     def post(self, request):
-        pass
+        storage_file(request.FILES['image'])
+        return render(request, 'gallery/load_file.html')
